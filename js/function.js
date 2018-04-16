7f59bfa04f8f7a4e5751d0ef97b6ab3aca51af46
hyncao <277148066@qq.com>
2018-04-16 12:14:51 +0800

    change to echart

--- js/function.js
+++ js/function.js
@@ -1,6 +1,33 @@
-var vm = new Vue({
-	el: '#index',
-	data: {
-		text: '这是第一个github.io'
+// var vm = new Vue({
+// 	el: '#index',
+// 	data: {
+// 		text: '这是第一个github.io'
+// 	}
+// })
+
+ajax('get','json/state.json',function(data){
+	console.log(data);
+	var myChart = echarts.init(document.getElementById('chart'));
+	var chartOption = {
+
 	}
+	myChart.setOption(chartOption);
 })
+
+let ajax = (type,url,callback) => {
+	var xhr;
+	if (window.XMLHttpRequest) {
+		xhr = window.XMLHttpRequest();
+	} else {
+		xhr = new ActiveXObject('Microsoft.XMLHTTP');
+	}
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState == 4) {
+			if (xhr.status == 200) {
+				callback(xhr.responseText);
+			}
+		}
+	}
+	xhr.open(type,url);
+	xhr.send();
+}
