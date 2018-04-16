5de9853ef94c0f9b3a8fa4cacbc07a16b33811e4
hyncao <277148066@qq.com>
2018-04-16 20:20:14 +0800

    bugfix

--- js/function.js
+++ js/function.js
@@ -4,20 +4,38 @@
 // 		text: '这是第一个github.io'
 // 	}
 // })
+window.onload = function(){
+	ajax('get','json/data.json',function(data){
+		data = JSON.parse(data);
+		var myChart = echarts.init(document.getElementById('chart'));
+		var chartOption = {
+			title: {
+				text: 'No fxxk'
+			},
+			tooltip: {
+				trigger: 'axis'
+			},
+			legend: {
+				data: getPeople(data)
+			},
+			xAxis: {
+				type: 'category',
+				boundaryGap: false,
+				data: getDate(data)
+			},
+			yAxis: {
+				type: 'value'
+			},
+			series: getSeries(data)
+		}
+		myChart.setOption(chartOption);
+	})
+}
 
-ajax('get','json/data.json',function(data){
-	console.log(data);
-	var myChart = echarts.init(document.getElementById('chart'));
-	var chartOption = {
-
-	}
-	myChart.setOption(chartOption);
-})
-
-var ajax = (type,url,callback) => {
-	var xhr;
+let ajax = (type,url,callback) => {
+	let xhr;
 	if (window.XMLHttpRequest) {
-		xhr = window.XMLHttpRequest();
+		xhr = new XMLHttpRequest();
 	} else {
 		xhr = new ActiveXObject('Microsoft.XMLHTTP');
 	}
@@ -28,6 +46,61 @@ var ajax = (type,url,callback) => {
 			}
 		}
 	}
-	xhr.open(type,url);
+	xhr.open(type,url+'?vision='+Math.random());
 	xhr.send();
+}
+
+let getPeople = data => {
+	var data = data.data;
+	let arr = [];
+	data.forEach(function(a){
+		arr.push(a.name);
+	})
+	return arr;
+}
+
+let getDate = data => {
+	var data = data.data;
+	let arr = [];
+	data.forEach(function(a){
+		const list = a.list;
+		list.forEach(function(b){
+			if (!arr.includes(b)) {
+				arr.push(b);
+			}
+		})
+	})
+	return arr;
+}
+
+let getSeries = data => {
+	var from = getDate(data), data = data.data;
+	let arr = [];
+	data.forEach(function(a){
+		let obj = {
+			name: a.name,
+			type: 'line',
+			data: getNum(a.list, from)
+		};
+		arr.push(obj);
+	})
+	return arr;
+}
+
+let getNum = (data, from) => {
+	let arr = [];
+	from.forEach(function(a){
+		if (data.includes(a)){
+			let count = 0;
+			data.forEach(function(b){
+				if (a === b) {
+					count ++;
+				}
+			})
+			arr.push(count);
+		} else {
+			arr.push(0);
+		}
+	})
+	return arr;
 }
